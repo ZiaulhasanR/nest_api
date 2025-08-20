0db43bee6869b5c1e91e137eb9f996e4a07d623a
ziaulhassan <ziaulhasanf@gmail.com>
2025-08-20 16:30:05 +0600

    param added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,24 @@
-import { Get,Controller, Param } from '@nestjs/common';
+import { Get,Controller, Param,Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    @Get()
-    getUsers() {
-        return this.userService.getAllUsers();
-    }
+    // @Get()
+    // getUsers() {
+    //     return this.userService.getAllUsers();
+    // }
     @Get(':id')
     getUserById(@Param('id') id: string) {
         return this.userService.getUserById(Number(id));
+    }
+    @Get()
+    getUserType(@Query('type') type: string) {
+        return [{type}];
+    }
+    @Get(':type')
+    getUsersByType(@Param('type') type: string) {
+        return this.userService.getUserType(type);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,14 +3,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
     private users = [
-        { id: 1, name: 'Ziaul', email: 'ziaul@gmail.com'},
-        { id: 2, name: 'John', email: 'John@gmail.com' },
-        { id: 3, name: 'Jane', email: 'jane@gmail.com' },
+        { id: 1, name: 'Ziaul', email: 'ziaul@gmail.com', type: 'admin' },
+        { id: 2, name: 'John', email: 'John@gmail.com', type: 'user' },
+        { id: 3, name: 'Jane', email: 'jane@gmail.com', type: 'user' },
     ];
     getAllUsers() {
         return this.users;
     }
     getUserById(id: number) {
         return this.users.find(user => user.id === id);
+    }
+    getUserType(type: string) {
+        return this.users.filter(user => user.type === type);
     }
 }
