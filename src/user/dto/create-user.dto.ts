f9498d521a8a9b1bd415c14ce7587c75c3e637d5
ziaulhassan <ziaulhasanf@gmail.com>
2025-09-04 17:34:13 +0600

    successfully create a crud alhamdulilah

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,34 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    IsNumber,
+    MinLength,
+    IsBoolean,
+    Matches
+} from "class-validator";
+
 export class CreateUserDto {
-  readonly name: string;
-  readonly email: string;
-  readonly password: string;
-}
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    @IsString()
+    @Matches(/^(?=.*[0-9])/, {
+        message: 'Password must contain at least one number',
+    })
+    readonly password: string;
+
+    // @IsNotEmpty()
+    // @IsNumber()
+    // age: string;
+
+    // @IsBoolean()
+    // isActive: boolean;
+}   
