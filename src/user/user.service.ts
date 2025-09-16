88ed555bf4407867877619566567faca33abdf99
ziaulhassan <ziaulhasanf@gmail.com>
2025-09-16 14:41:25 +0600

    enable CORS and simplify user creation logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,8 +12,7 @@ export class UserService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
+    return this.userModel.create(createUserDto);
   }
 
   async findAll(): Promise<User[]> {
